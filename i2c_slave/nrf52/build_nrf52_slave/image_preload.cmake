03af4bbb9edaa96e0a13b54962c25224a2f39f3f
ulisesDCM <ulises.cmorales@hotmail.com>
2025-02-06 20:08:41 +0200

    Remove build folder.

--- i2c_slave/nrf52/build_nrf52_slave/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk/nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/users/ulise/repos/-hadar_cohen_demo/i2c_slave/nrf52" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/Users/ulise/repos/-Hadar_Cohen_demo/i2c_slave/nrf52/boards/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
